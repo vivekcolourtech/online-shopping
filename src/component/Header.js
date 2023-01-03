4690db361c1f441c7ff326a4cab6adea7909efc6
vivekcolourtech <75770447+vivekcolourtech@users.noreply.github.com>
2023-01-03 20:14:43 +0530

    initial commit

--- src/component/Header.js
+++ src/component/Header.js
@@ -6,7 +6,9 @@ function Header() {
             <nav className="navbar navbar-expand-lg bg-success text-wight">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="#">Walmart</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
