37653e6986f93aef87ff65ffd3c0feb94605c0f1
vivekcolourtech <75770447+vivekcolourtech@users.noreply.github.com>
2023-01-03 20:53:54 +0530

    new one comment of home component

--- src/component/Home.js
+++ src/component/Home.js
@@ -33,7 +33,9 @@ function Home() {
                 <img src={hairphone} className="card-img-top" alt="..." />
                 <div className="card-body">
                   <h5 className="card-title">Card title</h5>
-                  <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <p className="card-text">
+                  This is a longer card with supporting text below as a natural lead-in to additional content.
+                  This content is a little bit longer.</p>
                 </div>
               </div>
             </div>
