b2cafbe3f1d75412b22c13b7a2c078276aba02d6
vivekcolourtech <75770447+vivekcolourtech@users.noreply.github.com>
2023-01-03 18:59:41 +0530

    home-page new component

---
+++ src/component/Header.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg bg-success text-wight">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Walmart</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Deportment</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Service</a>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Dropdown
+                                </a>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="#">Action</a></li>
+                                    <li><a className="dropdown-item" href="#">Another action</a></li>
+                                    <li><hr className="dropdown-divider" /></li>
+                                    <li><a className="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#" >Recorded Item</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#" >Sing In Account</a>
+                            </li>
+                        </ul>
+                        <form className="d-flex text-align:right" role="search">
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button className="btn btn-primari" type="submit">Search</button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/component/Home.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import shoes from '../component/Photes/shoes.jpg';
+import hairphone from '../component/Photes/hair phone.jpg';
+
+function Home() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-5">
+          <div className="list-group">
+            <a href="#" className="list-group-item list-group-item-action active" aria-current="true">
+              The current link item
+            </a>
+            <a href="#" className="list-group-item list-group-item-action">A second link item</a>
+            <a href="#" className="list-group-item list-group-item-action">A third link item</a>
+            <a href="#" className="list-group-item list-group-item-action">A fourth link item</a>
+            <a className="list-group-item list-group-item-action disabled">A disabled link item</a>
+          </div>
+        </div>
+        <div className="col-sm-7">
+          <div className="row row-cols-1 row-cols-md-3 g-4">
+            <div className="col">
+              <div className="card">
+                <img src={shoes} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+              </div>
+            </div>
+            <div className="col">
+              <div className="card">
+                <img src={hairphone} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+              </div>
+            </div>
+            <div className="col">
+              <div className="card">
+                <img src={shoes} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.ead-in to additional content.</p>
+                </div>
+              </div>
+            </div>
+            <div className="col">
+              <div className="card">
+                <img src={hairphone} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+              </div>
+            </div>
+            <div className="col">
+              <div className="card">
+                <img src={shoes} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+              </div>
+            </div>
+            <div className="col">
+              <div className="card">
+                <img src={shoes} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+              </div>
+            </div>
+            <div className="col">
+              <div className="card">
+                <img src={hairphone} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+              </div>
+            </div>
+            <div className="col">
+              <div className="card">
+                <img src={hairphone} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+              </div>
+            </div>
+            <div className="col">
+              <div className="card">
+                <img src={shoes} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
